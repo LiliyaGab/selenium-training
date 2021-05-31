c247bb060ecbeffa2a2d792627abf0248336d2f7
1 <2>
2021-05-31 23:10:56 +0300

    ProductTests added

---
+++ selenium-training-csharp/ProductTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace selenium_training_csharp
+{
+    [TestFixture]
+    public class ProductTests : TestBase
+    {
+        [Test]
+        public void StickersTest()
+        {
+            driver.Url = "http://localhost/litecart/";
+            var productLocator = By.CssSelector("li.product");
+            var stickerLocator = By.CssSelector("div.sticker");
+            var products = driver.FindElements(productLocator);
+            for (int i=0; i< products.Count; i++)
+            {
+                Assert.True(products[i].FindElements(stickerLocator).Count == 1);
+            }
+        }
+    }
+}
