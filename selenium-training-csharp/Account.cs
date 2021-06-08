3fab177bc028641999f1f8acfd3dac011f225770
1 <2>
2021-06-08 14:17:42 +0300

    homework 11 test added

---
+++ selenium-training-csharp/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_training_csharp
+{
+    public class Account
+    {
+        public Account()
+        {
+            FirstName = GenerateRandomString(10);
+            LastName = GenerateRandomString(10);
+            Address = GenerateRandomString(10);
+            City = GenerateRandomString(10);
+            Email = GenerateRandomString(6) + "@gmail.com";
+            Phone = "+" + GenerateRandomInt(11);
+            Postcode = GenerateRandomInt(5);
+            Password = GenerateRandomString(8);
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Postcode { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public static Random rnd = new Random();
+
+        public static string GenerateRandomString(int count)
+        {
+            char[] letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                builder.Append(letters[rnd.Next(0, letters.Length - 1)]);
+            }
+            return builder.ToString();
+        }
+        public static string GenerateRandomInt(int count)
+        {
+            char[] letters = "0123456789".ToCharArray();
+            var builder = new StringBuilder();
+            for (int i=0; i< count; i++)
+            {
+                builder.Append(letters[rnd.Next(0, letters.Length - 1)]);
+            }
+            return builder.ToString();
+        }
+    }
+}
--- selenium-training-csharp/LoginTests.cs
+++ selenium-training-csharp/LoginTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace selenium_training_csharp
 {
@@ -19,6 +20,35 @@ namespace selenium_training_csharp
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
             driver.FindElement(By.Name("login")).Click();           
+        }
+        public static Random rnd = new Random();
+        [Test]
+        public void RegistrationTest_HW11()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            var account = new Account();
+            driver.FindElement(By.XPath("//*[text()='New customers click here']")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Name("firstname")));
+            driver.FindElement(By.Name("firstname")).SendKeys(account.FirstName);
+            driver.FindElement(By.Name("lastname")).SendKeys(account.LastName);
+            driver.FindElement(By.Name("address1")).SendKeys(account.Address);
+            driver.FindElement(By.Name("postcode")).SendKeys(account.Postcode);
+            driver.FindElement(By.Name("city")).SendKeys(account.City);
+            var country = new SelectElement(driver.FindElement(By.Name("country_code")));
+            country.SelectByText("United States");
+            var zone = new SelectElement(driver.FindElement(By.CssSelector("select[name=zone_code]")));
+            zone.SelectByText("Alaska");
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+            driver.FindElement(By.Name("phone")).SendKeys(account.Phone);
+            driver.FindElement(By.Name("password")).SendKeys(account.Password);
+            driver.FindElement(By.Name("confirmed_password")).SendKeys(account.Password);
+            driver.FindElement(By.Name("create_account")).Click();
+            driver.FindElement(By.XPath("//*[text()='Logout']")).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Name("email")));
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+            driver.FindElement(By.Name("password")).SendKeys(account.Password);
+            driver.FindElement(By.Name("login")).Click();
+            driver.FindElement(By.XPath("//*[text()='Logout']")).Click();
         }
 
     }
